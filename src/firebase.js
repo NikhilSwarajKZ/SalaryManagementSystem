554b65c9354e18eaacb20fe5dc33195dd92026ae
Nikhil Swaraj <119269184+NikhilSwarajKZ@users.noreply.github.com>
2024-07-19 11:55:10 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import {getFirestore} from '@firebase/firestore'
 import {getStorage} from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCevfjxYh5AOuPpzdQmiqTJOyoDJHZRxRQ",
-  authDomain: "salarymanagementapp-bf7be.firebaseapp.com",
-  projectId: "salarymanagementapp-bf7be",
-  storageBucket: "salarymanagementapp-bf7be.appspot.com",
-  messagingSenderId: "278630086267",
-  appId: "1:278630086267:web:937597dc30e7b764dbd4de"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
